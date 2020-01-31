3952b288baced38df2819bcdd3c35d9e7b3cbbdc
ThePiGuy <ThePiGuy@users.noreply.github.com>
2020-01-31 15:30:07 -0700

    L2: Finished in lab code

--- Lab2/Lab2Ex2/src/Point.java
+++ Lab2/Lab2Ex2/src/Point.java
@@ -3,8 +3,8 @@ class Point {
 	private int x, y;
 
 	public Point(int x, int y) {
-	this.x = x;
-	this.y = y;
+		this.x = x;
+		this.y = y;
 	}
 	
 	static public double distance(Point a, Point b){
@@ -14,11 +14,10 @@ class Point {
 	}
 	
 	public String toString(){
-        // THIS METHOD DOESN'T WORK. AS PART OF EXERCISE-2 STUDENTS MUST FIX IT
-        // TO RETURN A STRING WITH THE COORDINATES OF A POINT IN THE FORMAT SHOWN
-        // IN THE EXAMPLE BELOW:
+        // Intended Format:
         // (20, 30)
-        String s = "Oops... Point's toString doesn't know how to display point coordinates.";
+		
+        String s = "(" + x + ", " + y + ")";
 		return  s;
 	}
 }
