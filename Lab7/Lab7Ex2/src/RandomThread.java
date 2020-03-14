ebb0c735979517f27fbf5290dc87f35f029937f5
ThePiGuy <ThePiGuy@users.noreply.github.com>
2020-03-14 16:08:20 -0600

    L7E2: Created basic structure

---
+++ Lab7/Lab7Ex2/src/RandomThread.java
@@ -0,0 +1,22 @@
+
+/**
+ * Implements a single runnable thread.
+ * 
+ * @author Parker Link
+ * @since Mar. 14, 2020
+ * @version 1.0.0
+ *
+ */
+public class RandomThread implements Runnable {
+
+	public RandomThread() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
